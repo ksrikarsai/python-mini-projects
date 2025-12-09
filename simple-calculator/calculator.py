2fc840d607e05ea9b3565ce87be945f552d74b02
Srikar Koneru <167067312+ksrikarsai@users.noreply.github.com>
2025-12-09 21:29:02 +0530

    Create calculator.py

---
+++ simple-calculator/calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter First Number "))
+choice = input("Enter Operation to be performed ")
+b = int(input("Enter Second Number "))
+if(choice=="+"):
+    print("The addition of given numbers is ",a+b)
+elif(choice=="*"):
+    print("The Multiplication of given numbers is ",a*b)
+elif(choice=="-"):
+    print("The subtraction of given numbers is ",a-b)
+elif(choice=="/"):
+    print("The division of two numbers is ",a/b)
